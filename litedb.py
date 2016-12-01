8dce0911b4f4d1399f59cf5777a851bde836a564
leafcoder <leafcoder@gmail.com>
2016-12-01 23:16:58 +0800

    修改访问接口

--- litedb.py
+++ litedb.py
@@ -1,98 +1,108 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+__version__ = '1.0'
 
 import re
 import yaml
 import sqlite3
 from cStringIO import StringIO
 
-__version__ = '1.0'
+max_limit = 100
 is_name = re.compile(r'^[a-zA-Z0-9]+$').match
 
 class LiteDB(object):
 
     def __init__(self, path):
         self.path = path
-        self.conn = self._connect()
-
-    def _connect(self):
-        conn = sqlite3.connect(self.path, check_same_thread=True)
+        self.conn = conn = sqlite3.connect(self.path, check_same_thread=True)
         conn.text_factory = str
+        conn.isolation_level=None
+        conn.execute('BEGIN;')
         conn.execute('PRAGMA foreign_keys = ON;')
         conn.execute('PRAGMA cache_size = 8000;')
         conn.commit()
-        return conn
+
+    def begin(self):
+        return LiteDBTransaction(self)
+
+class LiteDBTransaction(object):
+
+    def __init__(self, litedb):
+        self.litedb = litedb
+        self.conn = litedb.conn
+        self.conn.execute('BEGIN;')
 
     def commit(self):
         self.conn.commit()
+        self.conn.execute('BEGIN;')
 
     def rollback(self):
         self.conn.rollback()
+        self.conn.execute('BEGIN;')
 
-    @property
-    def tables(self):
+    def tables(self, offset=0, limit=10):
         conn = self.conn
+        offset = int(offset)
+        limit = min(int(limit), max_limit)
         cur = conn.execute('''\
         SELECT name FROM sqlite_master
         WHERE type='table' AND name like 'litedb_%'
-        ORDER BY name;''')
-        ret = cur.fetchall()
+        ORDER BY name LIMIT ? OFFSET ?;
+        ''', (limit, offset))
         names = []
-        for row in ret:
+        for row in cur.fetchall():
             name = row[0]
             if not name.startswith('litedb_'):
                 continue
             names.append(name[len('litedb_'):])
         return tuple(names)
 
-    def table(self, name, auto_increment=True):
-        self.create(name, auto_increment=auto_increment)
-        return LiteDBTable(
-            self, name, auto_increment=auto_increment
-        )
+    def table(self, name):
+        if not is_name(name):
+            raise ValueError('invalid table name %s' % name)
+        conn = self.conn
+        conn.execute('''\
+        CREATE TABLE IF NOT EXISTS litedb_%s (
+            id INTEGER,
+            data BYTEA,
+            PRIMARY KEY (id)
+        );''' % name)
+        return LiteDBTable(self, name)
+
+    def exists(self, name):
+        if not is_name(name):
+            raise ValueError('invalid table name %s' % name)
+        conn = self.conn
+        cur = conn.execute('''\
+        SELECT name FROM sqlite_master WHERE type='table' AND name=?;
+        ''', ('litedb_%s' % name, ))
+        ret = cur.fetchone()
+        if ret is None:
+            return False
+        return True
 
     def delete(self, name):
-        if is_name(name) is None:
-            raise ValueError('invalid table name')
+        if not is_name(name):
+            raise ValueError('invalid table name %s' % name)
         conn = self.conn
         conn.execute('''\
         DROP TABLE IF EXISTS litedb_%s;''' % name)
 
-    def create(self, name, auto_increment=True):
-        if is_name(name) is None:
-            raise ValueError('invalid table name')
-        conn = self.conn
-        if auto_increment:
-            conn.execute('''\
-            CREATE TABLE IF NOT EXISTS litedb_%s (
-                id INTEGER,
-                data BYTEA,
-                PRIMARY KEY (id)
-            )
-            ''' % name)
-        else:
-            conn.execute('''\
-            CREATE TABLE IF NOT EXISTS litedb_%s (
-                id VARCHAR,
-                data BYTEA,
-                PRIMARY KEY (id)
-            )
-            ''' % name)
-
 class LiteDBTable(object):
 
-    def __init__(self, litedb, name, auto_increment=True):
-        self.auto_increment = auto_increment
+    def __init__(self, txn, name):
+        self.txn = txn
         self.name = name
-        self.litedb = litedb
 
-    def rows(self):
-        litedb = self.litedb
-        conn = litedb.conn
+    def rows(self, offset=0, limit=10):
+        txn = self.txn
+        conn = txn.conn
+        offset = int(offset)
+        limit = min(int(limit), max_limit)
         cur = conn.execute('''\
-        SELECT id, data FROM litedb_%s;
-        ''' % self.name)
+        SELECT id, data FROM litedb_%s LIMIT ? OFFSET ?;
+        ''' % self.name, (limit, offset))
         result = []
         for id, data_str in cur.fetchall():
             data = data_str
@@ -101,40 +111,9 @@ class LiteDBTable(object):
             result.append(LiteDBRow(self, id, data))
         return result
 
-    def row(self, *args, **kwargs):
-        litedb = self.litedb
-        conn = litedb.conn
-        if 0 == len(args):
-            if 'data' in kwargs:
-                id = kwargs.get('id')
-                data = kwargs['data']
-                return self._new_row(id, data)
-            else:
-                id = kwargs.pop('id')
-                return self._get_row(id)
-        elif 1 == len(args):
-            id = args[0]
-            if 'data' not in kwargs:
-                return self._get_row(id)
-            data = kwargs['data']
-            return self._new_row(id, data)
-        elif 2 == len(args):
-            id, data = args
-            return self._new_row(id, data)
-        else:
-            id, data = args[0], args[1]
-            return self._new_row(id, data)
-
-    def delete(self, id):
-        litedb = self.litedb
-        conn = litedb.conn
-        cur = conn.execute('''\
-        DELETE FROM litedb_%s WHERE id=?;
-        ''' % self.name, (id, ))
-
-    def _get_row(self, id):
-        litedb = self.litedb
-        conn = litedb.conn
+    def row(self, id):
+        txn = self.txn
+        conn = txn.conn
         cur = conn.execute('''\
         SELECT id, data FROM litedb_%s WHERE id=?;
         ''' % self.name, (id, ))
@@ -147,32 +126,38 @@ class LiteDBTable(object):
             data = yaml.load(StringIO(data_str))
         return LiteDBRow(self, id, data)
 
-    def _new_row(self, id, data):
-        litedb = self.litedb
-        conn = litedb.conn
+    def insert(self, data):
+        txn = self.txn
+        conn = txn.conn
         data_str = data
         if data is not None:
             data_str = yaml.dump(data)
-        if id is not None:
-            if self.auto_increment:
-                try:
-                    id = int(id)
-                except:
-                    raise ValueError('id requires integer')
-            cur = conn.execute('''\
-            UPDATE litedb_%s SET data=? WHERE id=?;
-            ''' % self.name, (data_str, id))
-            if cur.rowcount == 0:
-                cur = conn.execute('''\
-                INSERT INTO litedb_%s (id, data) VALUES (?, ?);
-                ''' % self.name,  (id, data_str))
-                id = cur.lastrowid
-        else:
-            cur = conn.execute('''\
-            INSERT INTO litedb_%s (data) VALUES (?);
-            ''' % self.name,  (data_str, ))
-            id = cur.lastrowid
+        cur = conn.execute('''\
+        INSERT INTO litedb_%s (data) VALUES (?);
+        ''' % self.name,  (data_str, ))
+        id = cur.lastrowid
         return LiteDBRow(self, id, data)
+
+    def update(self, id, data):
+        txn = self.txn
+        conn = txn.conn
+        if isinstance(id, LiteDBRow):
+            id = id.id
+        data_str = data
+        if data is not None:
+            data_str = yaml.dump(data)
+        cur = conn.execute('''\
+        UPDATE litedb_%s SET data=? WHERE id=?;
+        ''' % self.name, (data_str, id))
+
+    def delete(self, id):
+        txn = self.txn
+        conn = txn.conn
+        if isinstance(id, LiteDBRow):
+            id = id.id
+        cur = conn.execute('''\
+        DELETE FROM litedb_%s WHERE id=?;
+        ''' % self.name, (id, ))
 
     # TODO
     def search(self, AND=None, OR=None, NOT=None, order=None):
